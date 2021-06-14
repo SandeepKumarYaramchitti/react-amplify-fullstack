b26844c845a1daec31c76c917214be3e4a6c3bdf
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-06-13 21:32:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Amplify, {API, graphqlOperation} from 'aws-amplify'
 import {createTodo} from './graphql/mutations'
-import {getTodo} from './graphql/queries'
+import {getTodo, listTodos} from './graphql/queries'
 import awsExports from './aws-exports'
 Amplify.configure(awsExports)
 
 
 function App() {
+
+  const [todos, settodos] = useState([])
+
+  useEffect(() => {
+    fetchTodos();
+  }, [])
+
+  async function fetchTodos() {
+      try {
+          const toDoData = await API.graphql(graphqlOperation(listTodos));
+          // @ts-ignore
+          const todos = toDoData.data.listTodos.items;
+          settodos(todos);
+
+      }catch (err) {
+
+      }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
